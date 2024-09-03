37090f78d2e62601fc4c89489e0db457d9732a36
DaniMori <DaniMori@users.noreply.github.com>
2024-09-03 14:21:46 +0200

    Adds unimported changes in branch "sharing"

--- R/Formulae.R
+++ R/Formulae.R
@@ -1220,3 +1220,7 @@ TRANSFORM_MATRIX_EXAMPLE_EQ <- latex_def(
   TRANSFORM_MATRIX_TRANSP_INV,
   TRANSFORM_MATRIX_EXAMPLE
 )
+
+### Application to examples from the literature: ----
+
+DISTANCE_AG_PARAM <- DISTANCE_SYM |> latex_sub(AGNOSTIC_SUBINDEX)
